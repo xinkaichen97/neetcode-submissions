1ca1bc72a848d4b1bdb19b5b2dee340a5621e205
Xinkai Chen <xinkaichen97@gmail.com>
2026-06-25 18:20:23 -0500

    Add: sql-top-travellers - submission-0

---
+++ SQL/sql-top-travellers/submission-0.sql
@@ -0,0 +1,6 @@
+SELECT u.name, COALESCE(SUM(r.distance), 0) AS travelled_distance
+FROM users u
+LEFT JOIN rides r 
+ON u.id = r.user_id
+GROUP BY u.id, u.name
+ORDER BY travelled_distance DESC, u.name ASC;
