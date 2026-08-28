c6aea73c8f00e73df4d5133130d6a17b79fd9699
Xinkai Chen <xinkaichen97@gmail.com>
2026-08-28 17:21:38 -0500

    Add: open-the-lock - submission-0

---
+++ Data Structures & Algorithms/open-the-lock/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        # base case
+        if "0000" in deadends:
+            return -1
+
+        # generate all 8 neighbors
+        def children(lock):
+            res = []
+            for i in range(4):
+                digit = str((int(lock[i]) + 1) % 10)
+                res.append(lock[:i] + digit + lock[i + 1:])
+                digit = str((int(lock[i]) - 1 + 10) % 10)
+                res.append(lock[:i] + digit + lock[i + 1:])
+            return res
+
+        # add 0000 to queue and deadends to visits
+        q = deque([("0000", 0)])
+        visit = set(deadends)
+
+        # bfs
+        while q:
+            lock, turns = q.popleft()
+            if lock == target:
+                return turns
+            for child in children(lock):
+                if child not in visit:
+                    visit.add(child)
+                    q.append((child, turns + 1))
+
+        return -1
