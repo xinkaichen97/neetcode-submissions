a17b4e33eef01504577ac58ab90671688dac731c
Xinkai Chen <xinkaichen97@gmail.com>
2026-09-09 18:47:01 -0500

    Add: integer-break - submission-1

---
+++ Data Structures & Algorithms/integer-break/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        # bottom-up DP
+        dp = [0] * (n + 1)
+        dp[1] = 1
+
+        for num in range(2, n + 1):
+            dp[num] = 0 if num == n else num
+            for i in range(1, num):
+                dp[num] = max(dp[num], dp[i] * dp[num - i])
+
+        return dp[n]
